71ada898eb2ed3b5692cd1d2f4ee04585a75f0a3
Afsan Noor Jim <150720347+afsanoorjim@users.noreply.github.com>
2025-11-06 10:15:50 +0000

    add

---
+++ silsila/user/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class UserProfile(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
